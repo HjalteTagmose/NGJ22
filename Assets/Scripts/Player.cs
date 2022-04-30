5c4dae2dbe59334ebb25fa56a7feb54cca518b1d
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2022-04-30 11:19:41 +0200

    add interaction system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;    
+    }
+    
+    void Update()
+    {
+        bool success = Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit);        
+        if (success)
+        {
+            var interactable = hit.transform.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                interactable.Hover();
+                if (Input.GetMouseButtonDown(0))
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+}
