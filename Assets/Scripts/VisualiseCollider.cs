092fd0fcea3b269029f27e220505222a317ec012
Jeppe Lindskov <jeppelind@icloud.com>
2022-04-30 19:53:40 +0200

    Tunne prefab wwise room + visualise collider

--- Assets/Scripts/VisualiseCollider.cs
+++ Assets/Scripts/VisualiseCollider.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 
 public class VisualiseCollider : MonoBehaviour
 {
-    public Color color = Color.cyan;
+    public Color color = new Color(0,255,255,0.3f); 
+    
+    
 
     private BoxCollider _collider; 
     private void OnDrawGizmos()
     {
         if (TryGetComponent(out _collider))
         {
+            Gizmos.matrix = transform.localToWorldMatrix; 
             Gizmos.color = color;
-            Gizmos.DrawCube(transform.position,transform.localScale);
+            Gizmos.DrawCube(Vector3.zero,Vector3.one);
         }
        
     }
