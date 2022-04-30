a8654327404cd9004fa3523ac24858148f1bc1e8
Jeppe Lindskov <jeppelind@icloud.com>
2022-04-30 22:52:52 +0200

    Wwise Room setup

--- Assets/Wwise/MonoBehaviour/Runtime/AkRoomPortal.cs
+++ Assets/Wwise/MonoBehaviour/Runtime/AkRoomPortal.cs
@@ -72,7 +72,7 @@ public class AkRoomPortal : AkTriggerHandler
 		}
 		else
 		{
-			UnityEngine.Debug.LogError(name + " has identical front and back rooms. It will not be sent to Spatial Audio.");
+			UnityEngine.Debug.LogError(name + " has identical front and back rooms. It will not be sent to Spatial Audio.",gameObject);
 			if (portalSet)
 				AkSoundEngine.RemovePortal(GetID());
 			portalSet = false;
