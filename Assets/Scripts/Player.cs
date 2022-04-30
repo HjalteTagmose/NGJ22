c51088d887f077ae46557061e8e042da3ec4742c
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2022-04-30 15:20:48 +0200

    make radiation 1-100

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float maxInteractLength = 10;
-    public float radioactiveTolerance = 1;
+    public float radioactiveTolerance = 100;
 
     private float radiation;
     private Camera cam;
